78b92d8f4cbdff06da18d755d31ae182a45e0fc4
Kiran KS <kirannick99@gmail.com>
2026-04-22 23:07:16 +0530

    added new private variable in entity

--- src/main/java/com/SBI/ATM/entity/Account.java
+++ src/main/java/com/SBI/ATM/entity/Account.java
@@ -7,10 +7,13 @@ import lombok.Data;
 @Table(name="SBI-USERS")
 @Data
 public class Account {
+
+
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name; 
+    private String name;
     private String gender;
     private double balance;
     private String creditCard;
